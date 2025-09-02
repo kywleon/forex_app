ef347266dffff5c70f8cf6e7393c777f8add8a76
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2025-09-02 11:51:55 +0800

    Update the code for test

--- backend/src/routes/externalRate.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getExchangeRates } from "../controllers/getExchangeRate.js";
-
-const router = express.Router();
-
-router.get("/", getExchangeRates);
-
-export default router;
--- backend/src/routes/ratesRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getRate } from "../controllers/ratesController.js";
-
-const router = express.Router();
-
-router.get("/", getRate);
-
-export default router;
